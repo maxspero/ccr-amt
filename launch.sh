9a565855a622ca614c6b0d476663b9fb95e46c54
Max Spero <mmspero@gmail.com>
2018-05-15 17:37:51 -0700

    add hit properties

--- launch.sh
+++ launch.sh
@@ -1,7 +1,6 @@
 python launch_hits.py \
-  --html_template=image_sentence.html \
-  --hit_properties_file=hit_properties/image_sentence.json \
-  --input_json_file=examples/image_sentence/example_input.txt \
-  --hit_ids_file=examples/image_sentence/hit_ids.txt
+  --html_template=hit_templates/ccr.html \
+  --hit_properties_file=hit_properties/ccr.json \
+  --hit_ids_file=output/hit_ids.txt
 
 
