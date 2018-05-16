a3f651e5a1c8a1bee1a5b5d4d4e83c38f5fdb942
Max Spero <mmspero@gmail.com>
2018-05-15 17:55:45 -0700

    working first draft of amt

---
+++ commands/get_results.sh
@@ -0,0 +1,4 @@
+python get_results.py \
+  --hit_ids_file=output/hit_ids.txt \
+  > output/results.txt
+
--- commands/launch.sh
+++ commands/launch.sh
@@ -1,6 +1,8 @@
 python launch_hits.py \
   --html_template=hit_templates/ccr.html \
   --hit_properties_file=hit_properties/ccr.json \
+  --input_json_file=empty_input.txt \
   --hit_ids_file=output/hit_ids.txt
+  
 
 
